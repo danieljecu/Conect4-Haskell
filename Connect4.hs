51f80479c6121f1b624c7fb53b28124cf3a874ae
Cătălin Jecu <catalin.jecu@gmail.com>
2016-06-14 12:09:32 +0300

    connect4 fara AI
    fara tratare de exceptii

---
+++ Connect4.hs
@@ -0,0 +1,70 @@
+import Data.Char
+import Data.List
+import System.IO
+
+--editare AFISARE 
+v2 = ["123456","4321","1234","","123456","0","xx0"]
+--prelucrare afisare
+line_fill []="_|_|_|_|_|_"
+line_fill (x:xs)
+	| l < 6 = intersperse '|' ( (x:xs) ++ replicate (6-l) '_' )
+	| otherwise = intersperse '|' (x:xs)
+	where l= length (x:xs)
+
+board_fill_endl []= "\n1|2|3|4|5|6" --board_fillnn
+board_fill_endl (y:ys)= line_fill y ++ "\n" ++ board_fill_endl ys
+
+board_disp [] = putStrLn $ board_fill_endl emptyBoardModel
+board_disp (z:zs) = putStrLn $ board_fill_endl (z:zs)
+----------------------
+--putStrLn $ board_fill_endl $ transpose ["xxxoxo","xooo","xoox","xxoo","xox","xoo","xxx"]
+--transpose ["xxxoxo","xooo","xoox","xxoo","xox","xoo","xxx"]=>["xxxxxxx","xooxoox","xoooxox","ooxo","x","o"]
+--xxxxxxx
+--xooxoox
+--xoooxox
+--ooxo__
+--x_____
+--o_____
+--1234567
+-----------
+-------------------------------
+copy c [] = c:[]
+copy c (x:xs)  =  (x:xs) ++ c:[]
+-------------------------------
+-- MOVE chr coloana
+add:: Char -> Int -> [String] -> [String]
+add c x  []= add c x emptyBoardModel
+add c x (y:ys) = ((take ( x-1 ) (y:ys)) ++ (copy c ((y:ys)!!(x-1))):[] )++ (drop (x) (y:ys))
+----------------------------
+
+emptyBoardModel = replicate 7 ""
+--let res = add (head ("abc")) (read ("1")::Int) (replicate 7 "")
+
+
+gameloop (x:xs) = do 
+	putStr "c4 MOVE >"
+	hFlush stdout
+	linie <- getLine
+	
+	let tokens = words linie
+	
+	if head tokens == "quit" 
+	then do
+		return ()
+	else do
+		if length tokens == 2 
+		then do
+			let result = add (head (tokens!!0)) (read (tokens!!1) :: Int) (x:xs)
+			--AFISARE
+			board_disp result
+			--LOOP
+			gameloop result
+		else do
+			return ()
+
+main = do 
+	putStrLn "Welcome to Connect 4"
+	putStrLn "Scrie simbolul de pus pe tabla si un numar de la 1 la 7"
+	board_disp emptyBoardModel
+	gameloop emptyBoardModel
+	putStrLn "Game Over"
