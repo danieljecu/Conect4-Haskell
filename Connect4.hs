c381f342727437cb246d2ac7aacfb296aef3d5c3
Cătălin Jecu <catalin.jecu@gmail.com>
2016-06-15 18:48:00 +0300

    AI rudimentar
    Posible moves
    Strategie AI 1/3 Mijloc
    Nu am starea de win
    NU am $ tabla plina---> stop joc
    Nu am transpose

--- Connect4.hs
+++ Connect4.hs
@@ -31,6 +31,12 @@ board_disp (z:zs) = putStrLn $ board_fill_endl (z:zs)
 copy c [] = c:[]
 copy c (x:xs)  =  (x:xs) ++ c:[]
 -------------------------------
+--
+possibleMoves bs = map fst $ filter (\(a , _)-> length (bs!!(a-1)) <6) $ zip [1..7] bs
+------
+--AI
+mijloc m@(x:xs) = head. drop ((length m) `div` 2) $ m
+------
 -- MOVE chr coloana
 add:: Char -> Int -> [String] -> [String]
 add c x  []= add c x emptyBoardModel
@@ -41,7 +47,10 @@ emptyBoardModel = replicate 7 ""
 --let res = add (head ("abc")) (read ("1")::Int) (replicate 7 "")
 
 
-gameloop (x:xs) = do 
+gameloop bs@(x:xs) = do 
+	
+	putStrLn "Alege una din optiunile disponibile>"
+	putStrLn $ show $ possibleMoves bs
 	putStr "c4 MOVE >"
 	hFlush stdout
 	linie <- getLine
@@ -54,17 +63,33 @@ gameloop (x:xs) = do
 	else do
 		if length tokens == 2 
 		then do
-			let result = add (head (tokens!!0)) (read (tokens!!1) :: Int) (x:xs)
-			--AFISARE
-			board_disp result
+			
 			--LOOP
-			gameloop result
+			if (read (tokens!!1) :: Int) `elem` (possibleMoves bs)
+			then do 
+				----------MY MOVE
+				let result = add (head (tokens!!0)) (read (tokens!!1) :: Int) (x:xs)
+				--is win E ?? printStrLn "E WIN" board_disp result return ()
+				----------AI MOVE
+				let result_and_aiMove = add 'A' (mijloc $ possibleMoves result) result
+				--is win A ??
+				
+				--AFISARE
+				board_disp result_and_aiMove
+				----------NEXT MOVE
+				gameloop result_and_aiMove
+				
+			else do 
+				putStrLn ""
+				putStrLn "Incearca dinnou"
+				gameloop (x:xs)
 		else do
 			return ()
 
 main = do 
 	putStrLn "Welcome to Connect 4"
 	putStrLn "Scrie simbolul de pus pe tabla si un numar de la 1 la 7"
+	putStrLn "Atentie se adauga de la dreapta l astanga nu de sus in jos"
 	board_disp emptyBoardModel
 	gameloop emptyBoardModel
 	putStrLn "Game Over"
